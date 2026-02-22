868516096b073af86c72e8abc6ee9aba22bae4b5
countercheck <mike.atlin@gmail.com>
2026-02-22 17:16:26 -0500

    feat: bootstrap infra/azure with terraform provider config

---
+++ infra/azure/providers.tf
@@ -0,0 +1,16 @@
+# infra/azure/providers.tf
+terraform {
+  required_version = ">= 1.9"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  # subscription_id is read from ARM_SUBSCRIPTION_ID environment variable.
+  # Set it before running terraform: export ARM_SUBSCRIPTION_ID="<your-sub-id>"
+}
